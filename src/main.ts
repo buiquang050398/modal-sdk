bfde34ae65288d7ed6d773989c693516a6acb77a
Quang Bui <bpdquang@tma.com.vn>
2023-10-16 09:51:47 +0700

    Create build workflow for npm packages

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+//import { SpecialUploaderPlugin } from "@brendan_bui/wallet-modal";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-
-const app = createApp(App)
-
-app.use(createPinia())
-
-app.mount('#app')
+const app = createApp(App);
+//app.use(SpecialUploaderPlugin)
+app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,26 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/wallet.ts'),
+      name: 'Wallet Modal',
+      fileName: 'wallet-modal'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
